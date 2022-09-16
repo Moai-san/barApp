76737dd338a856459bd1dfb8bdb0aab0c115d671
Moai-san <leonardo.gonzalez.c01@mail.pucv.cl>
2022-09-16 17:04:06 -0300

    Push inicial con menu y navBar a medio hacer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { menu } from './menu'
+import { navBar } from './navBar'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    menu,
+    navBar
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navBar/navBar.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component(
+{
+    selector: 'navBar',
+    templateUrl: './navBar.html',
+    styleUrls: ['./navBar.scss']
+})
+
+export class navBar
+{
+    
+}
